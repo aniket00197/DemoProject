c3362a4d93524da81a2486105fc4e705b1a41947
aniket00197 <aniket00197@hotmail.com>
2022-02-07 23:55:28 +0530

    Change 1

---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+public class Person {
+    int pid;
+    String name, address;
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
